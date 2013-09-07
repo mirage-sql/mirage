c70cd333cc1a4e1087a6cb3e24e69bc0fd83d3c6
Daisuke Miyamoto <dai.0304@gmail.com>
2013-09-07 13:39:22 +0900

    remove unused import statements.

--- src/main/java/jp/sf/amateras/mirage/bean/BeanDescImpl.java
+++ src/main/java/jp/sf/amateras/mirage/bean/BeanDescImpl.java
@@ -2,8 +2,6 @@ package jp.sf.amateras.mirage.bean;
 
 import java.lang.annotation.Annotation;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
