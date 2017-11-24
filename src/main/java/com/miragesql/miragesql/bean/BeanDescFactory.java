cfa1407e32768ab39d719f08d1d30ef7597d03b7
aadrian <a.adrian.tech@googlemail.com>
2017-11-24 19:19:22 +0100

    javadoc

--- src/main/java/com/miragesql/miragesql/bean/BeanDescFactory.java
+++ src/main/java/com/miragesql/miragesql/bean/BeanDescFactory.java
@@ -5,6 +5,9 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Factory class to build <code>BeanDesc</code> instances depending on the bean types.
+ */
 public class BeanDescFactory {
 
     private Map<Class<?>, BeanDesc> cacheMap = new ConcurrentHashMap<>();
@@ -23,6 +26,12 @@ public class BeanDescFactory {
         this.propertyExtractor = propertyExtractor;
     }
 
+    /**
+     * Constructs a bean descriptor out of an Object instance.
+     *
+     * @param obj the object to create the descriptor from (the object can also be a Map)
+     * @return a descriptor
+     */
     @SuppressWarnings("unchecked")
     public BeanDesc getBeanDesc(Object obj){
         if(obj instanceof Map){
@@ -32,6 +41,13 @@ public class BeanDescFactory {
         }
     }
 
+    /**
+     * Constructs a bean descriptor out of a class.
+     *
+     * @param clazz the class to create the descriptor from. For <code>Map.class</code>, <code>HashMap.class</code> and
+     *              <code>LinkedHashMap.class</code> no properties can be extracted, so this operations needs to be done later.
+     * @return a descriptor
+     */
     public BeanDesc getBeanDesc(Class<?> clazz){
         if(clazz == Map.class || clazz == HashMap.class || clazz == LinkedHashMap.class){
             return new MapBeanDescImpl();
