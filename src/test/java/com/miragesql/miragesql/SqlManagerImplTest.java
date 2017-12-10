bb2d5356dd8a979883d065ca207a502da391fbba
aadrian <a.adrian.tech@googlemail.com>
2017-12-10 16:29:54 +0100

    move unit tests that don't depend on the "miragesql-test" back here.

--- src/test/java/com/miragesql/miragesql/SqlManagerImplTest.java
+++ src/test/java/com/miragesql/miragesql/SqlManagerImplTest.java
@@ -3,11 +3,234 @@ package com.miragesql.miragesql;
 import com.miragesql.miragesql.annotation.Column;
 import com.miragesql.miragesql.annotation.Enumerated;
 import com.miragesql.miragesql.annotation.PrimaryKey;
+import com.miragesql.miragesql.exception.BreakIterationException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SqlManagerImplTest extends AbstractDatabaseTest {
+
+    public void testGetSingleResultByMap() throws Exception {
+        Book book = new Book();
+        book.name = "Mirage in Action";
+        book.author = "Naoki Takezoe";
+        book.price = 20;
+
+        sqlManager.insertEntity(book);
+
+        Map<String, String> param = new HashMap<>();
+        param.put("bookName", "Mirage in Action");
+
+        @SuppressWarnings("unchecked")
+        Map<String, Object> map = sqlManager.getSingleResult(
+                Map.class,
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectByBookName.sql"),
+                param);
+
+        assertEquals("Mirage in Action", map.get("bookName"));
+        assertEquals("Naoki Takezoe", map.get("author"));
+        assertEquals("20", map.get("price"));
+    }
+
+    public void testSelectIn() throws Exception {
+        {
+            Book book = new Book();
+            book.name = "Mirage in Action";
+            book.author = "Naoki Takezoe";
+            book.price = 20;
+
+            sqlManager.insertEntity(book);
+        }
+        {
+            Book book = new Book();
+            book.name = "Essential Mirage";
+            book.author = "Naoki Takezoe";
+            book.price = 30;
+
+            sqlManager.insertEntity(book);
+        }
+
+        BookNamesParam param = new BookNamesParam();
+        param.bookNames.add("Mirage in Action");
+        param.bookNames.add("Essential Mirage");
+
+        List<Book> results = sqlManager.getResultList(
+                Book.class,
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectByBookNames.sql"),
+                param);
+
+        assertEquals(2, results.size());
+        assertEquals("Mirage in Action", results.get(0).name);
+        assertEquals("Essential Mirage", results.get(1).name);
+    }
+
+    public void testGetSingleResultByInteger() throws Exception {
+        Book book = new Book();
+        book.name = "Mirage in Action";
+        book.author = "Naoki Takezoe";
+        book.price = 20;
+
+        sqlManager.insertEntity(book);
+        sqlManager.insertEntity(book);
+
+        Integer count = sqlManager.getSingleResult(
+                Integer.class,
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_countBook.sql"));
+
+        assertEquals(2, count.intValue());
+    }
+
+    public void testGetCount() throws Exception {
+        Book book1 = new Book();
+        book1.name = "Mirage in Action";
+        book1.author = "Naoki Takezoe";
+        book1.price = 20;
+
+        Book book2 = new Book();
+        book2.name = "Mirage in Action";
+        book2.author = "Naoki Takezoe";
+        book2.price = 20;
+
+        sqlManager.insertEntity(book1);
+        sqlManager.insertEntity(book2);
+        assertEquals(new Long(0), book1.bookId);
+        assertEquals(new Long(1), book2.bookId);
+
+        Map<String, String> param = new HashMap<>();
+        param.put("bookName", "Mirage in Action");
+
+        int count = sqlManager.getCount(
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectByBookName.sql"),
+                param);
+
+        assertEquals(2, count);
+    }
+
+    public void testInsertEntity() throws Exception {
+        Book book = new Book();
+        book.name = "Mirage in Action";
+        book.author = "Naoki Takezoe";
+        book.price = 20;
+
+        sqlManager.insertEntity(book);
+        assertEquals(new Long(0), book.bookId);
+
+        BookParam param = new BookParam();
+        param.bookName = "Mirage in Action";
+
+        List<Book> bookList = sqlManager.getResultList(
+                Book.class,
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectByBookName.sql"),
+                param);
+
+        assertEquals(1, bookList.size());
+        assertEquals("Mirage in Action", bookList.get(0).name);
+        assertEquals("Naoki Takezoe", bookList.get(0).author);
+        assertEquals(new Integer(20), bookList.get(0).price);
+    }
+
+    public void testInsertEntity_Sequence() throws Exception {
+        UserInfo userInfo1 = new UserInfo();
+        userInfo1.userName = "Naoki Takezoe";
+
+        UserInfo userInfo2 = new UserInfo();
+        userInfo2.userName = "Coharu Takezoe";
+
+        sqlManager.insertEntity(userInfo1);
+        sqlManager.insertEntity(userInfo2);
+        assertEquals(new Long(0), userInfo1.userId);
+        assertEquals(new Long(1), userInfo2.userId);
+
+        List<UserInfo> userList = sqlManager.getResultList(
+                UserInfo.class,
+                new StringSqlResource("SELECT * FROM USER_INFO ORDER BY USER_ID"));
+
+        assertEquals(2, userList.size());
+        assertEquals(new Long(0), userList.get(0).userId);
+        assertEquals("Naoki Takezoe", userList.get(0).userName);
+        assertEquals(new Long(1), userList.get(1).userId);
+        assertEquals("Coharu Takezoe", userList.get(1).userName);
+    }
+
+    public void testInsertEntityWithNullProperty() throws Exception {
+        Book book = new Book();
+        book.name = "Mirage in Action";
+        book.author = "Naoki Takezoe";
+        book.price = null;
+
+        sqlManager.insertEntity(book);
+        assertEquals(new Long(0), book.bookId);
+
+        BookParam param = new BookParam();
+        param.bookName = "Mirage in Action";
+
+        List<Book> bookList = sqlManager.getResultList(
+                Book.class,
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectByBookName.sql"),
+                param);
+
+        assertEquals(1, bookList.size());
+        assertEquals("Mirage in Action", bookList.get(0).name);
+        assertEquals("Naoki Takezoe", bookList.get(0).author);
+        assertNull(bookList.get(0).price);
+    }
+
+    public void testInsertEntityWithEnumerated() throws Exception {
+        Magazine mag = new Magazine();
+        mag.name = "Mirage News";
+        mag.magType = Magazine.MagazineType.TYPE_B;
+        mag.price = 123;
+
+        sqlManager.insertEntity(mag);
+        assertEquals(new Long(0), mag.magazineId);
+
+        BookParam param = new BookParam();
+        param.bookName = "Mirage News";
+
+        List<Magazine> bookList = sqlManager.getResultList(
+                Magazine.class,
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectMagazineByBookName.sql"),
+                param);
+
+        assertEquals(1, bookList.size());
+        assertEquals("Mirage News", bookList.get(0).name);
+        assertEquals(Magazine.MagazineType.TYPE_B, bookList.get(0).magType);
+        assertEquals(Integer.valueOf(123), bookList.get(0).price);
+    }
+
+    public void testInsertBatchArray() throws Exception {
+        Book book1 = new Book();
+        book1.name = "Mirage in Action";
+        book1.author = "Naoki Takezoe";
+        book1.price = 20;
+
+        Book book2 = new Book();
+        book2.name = "Mastering Seasar2";
+        book2.author = "Naoki Takezoe";
+        book2.price = 25;
+
+        int result = sqlManager.insertBatch(book1, book2);
+        assertEquals(2, result);
+        assertEquals(new Long(0), book1.bookId);
+        assertEquals(new Long(1), book2.bookId);
+
+        List<Book> bookList = sqlManager.getResultList(
+                Book.class,
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectByBookName.sql"),
+                new BookParam());
+
+        assertEquals(2, bookList.size());
+
+        assertEquals("Mirage in Action", bookList.get(0).name);
+        assertEquals("Naoki Takezoe", bookList.get(0).author);
+        assertEquals(new Integer(20), bookList.get(0).price);
+
+        assertEquals("Mastering Seasar2", bookList.get(1).name);
+        assertEquals("Naoki Takezoe", bookList.get(1).author);
+        assertEquals(new Integer(25), bookList.get(1).price);
+    }
 
     public void testInsertBatchList() throws Exception {
         Book book1 = new Book();
@@ -41,6 +264,143 @@ public class SqlManagerImplTest extends AbstractDatabaseTest {
         assertEquals("Mastering Seasar2", bookList.get(1).name);
         assertEquals("Naoki Takezoe", bookList.get(1).author);
         assertEquals(new Integer(25), bookList.get(1).price);
+    }
+
+    public void testUpdateEntity() throws Exception {
+        Book book = new Book();
+        book.name = "Mirage in Action";
+        book.author = "Naoki Takezoe";
+        book.price = 20;
+
+        sqlManager.insertEntity(book);
+
+        BookParam param = new BookParam();
+        param.bookName = "Mirage in Action";
+
+        book = sqlManager.getSingleResult(
+                Book.class,
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectByBookName.sql"),
+                param);
+
+        book.price = 100;
+        sqlManager.updateEntity(book);
+
+        List<Book> bookList = sqlManager.getResultList(
+                Book.class,
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectByBookName.sql"));
+
+        assertEquals(1, bookList.size());
+        assertEquals("Mirage in Action", bookList.get(0).name);
+        assertEquals("Naoki Takezoe", bookList.get(0).author);
+        assertEquals(new Integer(100), bookList.get(0).price);
+    }
+
+    public void testDeleteEntity() throws Exception {
+        Book book = new Book();
+        book.name = "Mirage in Action";
+        book.author = "Naoki Takezoe";
+        book.price = 20;
+
+        sqlManager.insertEntity(book);
+
+        BookParam param = new BookParam();
+        param.bookName = "Mirage in Action";
+
+        book = sqlManager.getSingleResult(
+                Book.class,
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectByBookName.sql"),
+                param);
+
+        sqlManager.deleteEntity(book);
+
+        book = sqlManager.getSingleResult(
+                Book.class,
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectByBookName.sql"),
+                param);
+
+        assertNull(book);
+    }
+
+    public void testFindEntity(){
+        Book book = new Book();
+        book.name = "Mirage in Action";
+        book.author = "Naoki Takezoe";
+        book.price = 20;
+
+        sqlManager.insertEntity(book);
+
+        book = sqlManager.findEntity(Book.class, 0);
+        assertEquals("Mirage in Action", book.name);
+        assertEquals("Naoki Takezoe", book.author);
+        assertEquals(new Integer(20), book.price);
+
+        book = sqlManager.findEntity(Book.class, 1);
+        assertNull(book);
+    }
+
+    public void testIterate() throws Exception {
+        sqlManager.insertEntity(new Book("Mirage in Action", "Naoki Takezoe", 20));
+        sqlManager.insertEntity(new Book("Click in Action", "Bob Schellink", 30));
+        sqlManager.insertEntity(new Book("Introduction of Seasar2", "Naoki Takezoe", 30));
+
+        Integer result = sqlManager.iterate(
+                Book.class,
+                new IterationCallback<Book, Integer>() {
+
+                    private int result;
+
+                    public Integer iterate(Book entity) {
+                        result = result + entity.price;
+                        return result;
+                    }
+                },
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectByBookName.sql"));
+
+        assertEquals(80, result.intValue());
+    }
+
+    public void testIterateBreak() throws Exception {
+        sqlManager.insertEntity(new Book("Mirage in Action", "Naoki Takezoe", 20));
+        sqlManager.insertEntity(new Book("Click in Action", "Bob Schellink", 30));
+        sqlManager.insertEntity(new Book("Introduction of Seasar2", "Naoki Takezoe", 30));
+
+        Integer result = sqlManager.iterate(
+                Book.class,
+                new IterationCallback<Book, Integer>() {
+
+                    private int result;
+
+                    public Integer iterate(Book entity) {
+                        if(result >= 50){
+                            throw new BreakIterationException();
+                        }
+                        result = result + entity.price;
+                        return result;
+                    }
+                },
+                new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_selectByBookName.sql"));
+
+        assertEquals(50, result.intValue());
+    }
+
+    public void testCall() throws Exception {
+        try {
+            sqlManager.executeUpdate(new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_testCall_setUp.sql"));
+
+            NewBookParam newBookParam = new NewBookParam();
+            newBookParam.bookName = "Seasar2徹底入門";
+            newBookParam.author = "Naoki Takezoe";
+            newBookParam.price = 3990;
+            sqlManager.call("NEW_BOOK", newBookParam);
+
+            Book book = sqlManager.findEntity(Book.class, 0);
+            assertEquals("Seasar2徹底入門", book.name);
+            assertEquals("Naoki Takezoe", book.author);
+            assertEquals(3990, book.price.intValue());
+
+        } finally {
+            sqlManager.executeUpdate(new ClasspathSqlResource(SQL_PREFIX + "SqlManagerImplTest_testCall_tearDown.sql"));
+        }
     }
 
     public static class BookParam {
